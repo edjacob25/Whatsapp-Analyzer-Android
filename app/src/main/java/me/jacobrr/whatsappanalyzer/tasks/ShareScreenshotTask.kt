c186daa6786ecc17f368cd5012b87315e5b9a0dd
Jacob Rivera <edjacob25@gmail.com>
2019-12-09 23:57:28 -0600

    Updated form of how to save screenshots of the app

--- app/src/main/java/me/jacobrr/whatsappanalyzer/tasks/ShareScreenshotTask.kt
+++ app/src/main/java/me/jacobrr/whatsappanalyzer/tasks/ShareScreenshotTask.kt
@@ -1,71 +1,72 @@
 package me.jacobrr.whatsappanalyzer.tasks
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.AsyncTask
-import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import androidx.core.view.drawToBitmap
+import java.io.*
 
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
 import java.util.Date
 
 /**
  * Created by jacob on 24/11/2015.
  */
-class ShareScreenshotTask(private val myAct: Activity) : AsyncTask<Void, Void, String>() {
+class ShareScreenshotTask(private val myAct: Activity) : AsyncTask<Void, Void, Uri>() {
 
-    override fun doInBackground(vararg params: Void): String {
+    override fun doInBackground(vararg params: Void): Uri {
         return saveBitmap()
     }
 
-    override fun onPostExecute(aVoid: String) {
+    override fun onPostExecute(aVoid: Uri) {
         super.onPostExecute(aVoid)
         sendIntent(aVoid)
     }
 
-    fun saveBitmap(): String {
-        val myBitmap: Bitmap
+    private fun saveBitmap(): Uri {
         val v1 = myAct.findViewById<View>(android.R.id.content) //this works too
-        v1.isDrawingCacheEnabled = true
-        myBitmap = v1.drawingCache
+        val myBitmap = v1.drawToBitmap()
         val now = Date()
-        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
-        val dir = File(Environment.getExternalStorageDirectory().toString()
-                + File.separator + "WhatsAppAnalyzer")
-        if (!dir.exists())
-            dir.mkdir()
-        val filePath = (Environment.getExternalStorageDirectory().toString()
-                + File.separator + "WhatsAppAnalyzer" + File.separator + now + ".png")
-        val imagePath = File(filePath)
-        val fos: FileOutputStream
+        val name = android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
+
+        val resolver = myAct.applicationContext.contentResolver
+
+        val imageCollection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        val imageData = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "$name")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Screenshots/")
+            put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+        val uri = resolver.insert(imageCollection, imageData)
+
         try {
-            fos = FileOutputStream(imagePath)
-            myBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
-            fos.flush()
-            fos.close()
+            resolver.openOutputStream(uri!!).use {out ->
+                myBitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            }
+
+            imageData.clear()
+            imageData.put(MediaStore.Images.Media.IS_PENDING, 0)
+            resolver.update(uri, imageData, null, null)
         } catch (e: FileNotFoundException) {
             Log.e("GREC", e.message, e)
         } catch (e: IOException) {
             Log.e("GREC", e.message, e)
         }
-
-        v1.isDrawingCacheEnabled = false
-        return filePath
+        return uri!!
     }
 
-    fun sendIntent(path: String) {
-        val intent = Intent(android.content.Intent.ACTION_SEND)
-        intent.putExtra(android.content.Intent.EXTRA_TEXT,
+    fun sendIntent(path: Uri) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT,
                 "Generated with Whatsapp Analyzer")
         intent.type = "image/png"
-        val myUri = Uri.parse("file://$path")
-        intent.putExtra(Intent.EXTRA_STREAM, myUri)
+        intent.putExtra(Intent.EXTRA_STREAM, path)
         myAct.startActivity(Intent.createChooser(intent, "Send image"))
     }
 }
