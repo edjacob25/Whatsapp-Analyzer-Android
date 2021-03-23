fb2fb6714d42e8efb6ba3ca9ba959a48ab5aeaa9
Jacob Rivera <edjacob25@gmail.com>
2021-03-22 18:29:11 -0600

    Added small unit test

--- app/src/test/java/me/jacobrr/whatsappanalyzer/ExampleUnitTest.kt
+++ app/src/test/java/me/jacobrr/whatsappanalyzer/ExampleUnitTest.kt
@@ -1,10 +1,10 @@
 package me.jacobrr.whatsappanalyzer
 
 import me.jacobrr.whatsappanalyzer.logic.LineAnalyzer
-
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.time.LocalTime
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -12,7 +12,15 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     @Throws(Exception::class)
-    fun addition_isCorrect() {
-        assertEquals("Afternoon", LineAnalyzer().getTimeOfTheDay(12))
+    fun timeOfTheDayTest() {
+        assertEquals("Afternoon", LineAnalyzer().getTimeOfTheDay(LocalTime.of(12, 0)))
+    }
+
+    @Test
+    fun analyzedLineTest() {
+        val message = "19/01/18 10:36 AM - Jacob Rivera: soccer - Match Thread: Toluca vs São Paulo [Copa Libertadores]"
+        val analyzer = LineAnalyzer()
+        val line = analyzer.analyzeLine(message)
+        assertNotNull(line)
     }
 }
